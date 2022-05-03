a9ead55b8bf38b4e79d44d5f8bf3a1b9ab33e278
ozayca <65116709+ozayca@users.noreply.github.com>
2022-05-03 20:16:23 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@ import setuptools
 from setuptools import setup
 
 setup(name='gym_RLcourse',
-      author='Ayca Özcelikkale',
       url='https://github.com/ozayca/gym-RLcourse',
       packages=setuptools.find_packages(),
       install_requires=['gym']
