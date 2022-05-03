ccb59cb23533a7da7d711f57a4570d0f133aec38
David Widmann <devmotion@users.noreply.github.com>
2022-05-03 08:56:21 +0200

    Use `find_packages`

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
+import setuptools
 from setuptools import setup
 
 setup(name='gym_RLcourse',
       author='Ayca Özcelikkale',
       url='https://github.com/ozayca/gym-RLcourse',
-      packages=["gym_RLcourse"],
+      packages=setuptools.find_packages(),
       install_requires=['gym']
 )
